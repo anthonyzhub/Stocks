231f39b003ce8b21372ceb3a82d9c3e577b35ac0
Anthony <zamora.anthony1234@gmail.com>
2021-04-24 13:58:52 -0400

    Can save a graph of symbol's closing price

--- yFinance Test.py
+++ yFinance Test.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
 
 import yfinance as yf
+import pandas as pd
 import matplotlib.pyplot as plt
 
-def printHistory(stock):
+def printHistory(stock, symbol):
 
     historicalData = stock.history(period="1y") # <class 'pandas.core.frame.DataFrame'>
-    print(historicalData)
 
-    historicalData.plot(x="Close", y="Date")
-    plt.show()
+    historicalData["Close"].plot()
+    plt.savefig("{} Graph.jpg".format(symbol))
 
 def printDictionary(stock):
     
@@ -18,4 +18,4 @@ def printDictionary(stock):
 
 msft = yf.Ticker("MSFT")
 # printDictionary(msft.info)
-printHistory(msft)
+printHistory(msft, "MSFT")
