1d9457e458e5416e030812b844d4dacee6bbaed7
Anthony <zamora.anthony1234@gmail.com>
2021-04-26 17:57:57 -0400

    Adjustment

--- yFinance Test.py
+++ yFinance Test.py
@@ -3,7 +3,6 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 def printHistory(stock, symbol):
 
@@ -17,13 +16,6 @@ def printDictionary(stock):
     for key in sorted(stock.keys()):
         print("{}: {}".format(key, stock[key]))
 
-def plotlyExample(tickerSymbol):
-
-    df = px.data.stocks()
-    fig = px.line(df, x='date', y='{}'.format(tickerSymbol)) # <= https://plotly.com/python-api-reference/generated/plotly.express.line.html
-    fig.show()
-
 msft = yf.Ticker("MSFT")
 # printDictionary(msft.info)
-# printHistory(msft, "MSFT")
-plotlyExample("AAPL")
+printHistory(msft, "MSFT")
